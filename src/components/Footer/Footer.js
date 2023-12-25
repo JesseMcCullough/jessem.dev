d4bf93f936b710a2c3055d08228c5350219c893d
Jesse McCullough <jesse@jessem.dev>
2023-12-25 17:19:58 -0500

    Redesigned Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,26 +8,41 @@ export default function Footer() {
             <div className={`${styles.container} container`}>
                 <div className={styles["footer-text"]}>
                     <div className={styles["name-container"]}>
-                        <span className={`${styles.name} highlight`}>Jesse McCullough</span>
-                        <span className="title">Software Developer</span>
+                        <span className={`${styles.name} highlight`}>
+                            Jesse McCullough
+                        </span>
                     </div>
                     <ul>
-                        <li><a href="#portfolio">Portfolio</a></li>
-                        <li><a href="#skills">Skills</a></li>
-                        <li><a href="#about">About</a></li>
+                        <li>
+                            <a href="#portfolio">Portfolio</a>
+                        </li>
+                        <li>
+                            <a href="#skills">Skills</a>
+                        </li>
+                        <li>
+                            <a href="#about">About</a>
+                        </li>
                     </ul>
                 </div>
                 <div className={styles.icons}>
-                    <a href="https://www.instagram.com/jessemccullough" 
-                        target="_blank" rel="noreferrer">
+                    <a
+                        href="https://www.instagram.com/jessemccullough"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
                         <img src={instagramIcon} alt="" />
                     </a>
-                    <a href="https://github.com/jessemccullough"
-                        target="_blank" rel="noreferrer">
+                    <a
+                        href="https://github.com/jessemccullough"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
                         <img src={githubIcon} alt="" />
                     </a>
                 </div>
-                <p className={styles.copyright}>Copyright &copy; 2023 Jesse McCullough. All Rights Reserved.</p>
+                <p className={styles.copyright}>
+                    Copyright &copy; 2023 Jesse McCullough. All Rights Reserved.
+                </p>
             </div>
         </footer>
     );
