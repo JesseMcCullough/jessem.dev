7ef8e718577c8f98884cb66620414fc0f4f2813c
Jesse McCullough <jesse@jessem.dev>
2023-12-28 16:47:00 -0500

    Added logo to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,29 +1,24 @@
 import styles from "./Footer.module.css";
 import instagramIcon from "../../images/icons/instagram-icon.png";
 import githubIcon from "../../images/icons/github-icon.png";
+import logo from "../../images/logo.jpg";
 
 export default function Footer() {
     return (
         <footer>
             <div className={`${styles.container} container`}>
-                <div className={styles["footer-text"]}>
-                    <div className={styles["name-container"]}>
-                        <span className={`${styles.name} highlight`}>
-                            Jesse McCullough
-                        </span>
-                    </div>
-                    <ul>
-                        <li>
-                            <a href="#portfolio">Portfolio</a>
-                        </li>
-                        <li>
-                            <a href="#skills">Skills</a>
-                        </li>
-                        <li>
-                            <a href="#about">About</a>
-                        </li>
-                    </ul>
-                </div>
+                <img className={styles.logo} src={logo} alt="" />
+                <ul>
+                    <li>
+                        <a href="#portfolio">Portfolio</a>
+                    </li>
+                    <li>
+                        <a href="#skills">Skills</a>
+                    </li>
+                    <li>
+                        <a href="#about">About</a>
+                    </li>
+                </ul>
                 <div className={styles.icons}>
                     <a
                         href="https://www.instagram.com/jessemccullough"
