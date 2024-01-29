3ee0b339c3fed8b6e16b2a121ffb1aa042ab5025
Jesse McCullough <jesse@jessem.dev>
2024-01-29 15:46:11 -0500

    Add Portfolio and Project Strapi support

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,14 +1,26 @@
 import { useState, useRef, createRef } from "react";
 import Project from "./Project/Project";
 import styles from "./Portfolio.module.css";
-import portfolioData from "../../portfolioData.js";
+// import portfolioData from "../../portfolioData.js";
+import Markdown from "react-markdown";
+import { useStrapi, getStrapiUrl } from "../../hooks/useStrapi";
 
 export default function Portfolio() {
     const [currentProject, setCurrentProject] = useState(0);
     const projectsRef = useRef([]);
     const scrollContainer = useRef();
+    const { loading, error, data } = useStrapi(
+        "/home?populate[portfolio][populate][0]=project.images&populate[portfolio][populate][1]=project.technologies&populate[portfolio][populate][2]=project.button"
+    );
 
-    projectsRef.current = portfolioData.map(
+    if (loading) return;
+    if (error) return;
+
+    const portfolio = data.data.attributes.portfolio;
+    const title = portfolio.title;
+    const projects = portfolio.project;
+
+    projectsRef.current = projects.map(
         (_, index) => projectsRef.current[index] ?? createRef()
     );
 
@@ -49,17 +61,7 @@ export default function Portfolio() {
     return (
         <section className={styles.portfolio} id="portfolio">
             <div className="container">
-                <h2>
-                    I make{" "}
-                    <span className="highlight">data-driven web apps</span>,
-                    <br />
-                    <span className="highlight">
-                        full-stack responsive websites
-                    </span>
-                    ,
-                    <br />
-                    and <span className="highlight">REST APIs</span>.
-                </h2>
+                <Markdown components={{ p: "h2" }}>{title}</Markdown>
             </div>
 
             <div className={styles.grid}>
@@ -77,11 +79,11 @@ export default function Portfolio() {
                         &lt;
                     </div>
                 )}
-                {currentProject < portfolioData.length - 1 && (
+                {currentProject < projects.length - 1 && (
                     <div
                         className={styles["right-arrow"]}
                         onClick={() => {
-                            if (currentProject + 1 >= portfolioData.length) {
+                            if (currentProject + 1 >= projects.length) {
                                 return;
                             }
 
@@ -92,20 +94,35 @@ export default function Portfolio() {
                     </div>
                 )}
                 <div className={styles.container} ref={scrollContainer}>
-                    {portfolioData.map((project, index) => {
+                    {projects.map((project, index) => {
                         const onClick =
                             window.innerWidth > 1100
                                 ? () => onClickProject(index)
                                 : () => "";
+                        const images = [];
+                        if (project.images.data) {
+                            for (let image of project.images.data) {
+                                images.push(getStrapiUrl(image.attributes.url));
+                            }
+                        }
+
+                        // const technologies = {
+                        //     HTML: project.HTML,
+                        //     CSS: project.CSS,
+                        //     JavaScript: project.JavaScript,
+                        //     React: project.React,
+                        //     PHP: project.PHP,
+                        //     MySQL: project.MySQL,
+                        // };
+
                         return (
                             <Project
                                 key={index}
                                 name={project.name}
-                                technologyImages={project.technologyImages}
-                                projectImages={project.projectImages}
+                                technologies={project.technologies}
+                                projectImages={images}
                                 description={project.description}
-                                githubLink={project.githubLink}
-                                demoLink={project.demoLink}
+                                buttonData={project.button}
                                 ref={projectsRef.current[index]}
                                 onClick={onClick}
                             />
--- src/components/Portfolio/Project/Project.js
+++ src/components/Portfolio/Project/Project.js
@@ -1,20 +1,35 @@
 import { forwardRef } from "react";
 import ImageSlider from "../../ImageSlider/ImageSlider";
+import Button from "../../Button/Button";
 import styles from "./Project.module.css";
+import reactLogo from "../../../images/icons/react-logo.png";
+import cssLogo from "../../../images/icons/css-logo.png";
+import phpLogo from "../../../images/icons/php-logo.png";
+import htmlLogo from "../../../images/icons/html-logo.png";
+import javascriptLogo from "../../../images/icons/javascript-logo.png";
+import mysqlLogo from "../../../images/icons/mysql-logo.png";
+
+const supportedTechnologies = new Map();
+supportedTechnologies.set("HTML", htmlLogo);
+supportedTechnologies.set("CSS", cssLogo);
+supportedTechnologies.set("JavaScript", javascriptLogo);
+supportedTechnologies.set("React", reactLogo);
+supportedTechnologies.set("PHP", phpLogo);
+supportedTechnologies.set("MySQL", mysqlLogo);
 
 const Project = forwardRef(
     (
-        {
-            name,
-            technologyImages,
-            projectImages,
-            description,
-            githubLink,
-            demoLink,
-            onClick,
-        },
+        { name, technologies, projectImages, description, buttonData, onClick },
         ref
     ) => {
+        const technologyImages = [];
+        for (let technology in technologies) {
+            if (technologies[technology]) {
+                // supportedTechnologies' keys are the same as technologies' properties.
+                technologyImages.push(supportedTechnologies.get(technology));
+            }
+        }
+
         return (
             <div className={styles.project} onClick={onClick} ref={ref}>
                 <h3>{name}</h3>
@@ -28,14 +43,15 @@ const Project = forwardRef(
                 <ImageSlider projectImages={projectImages} />
 
                 <div className={styles.buttons}>
-                    <a
+                    {/* <a
                         className={styles.demo}
                         href={demoLink}
                         target="_blank"
                         rel="noreferrer"
                     >
                         Live Demo
-                    </a>
+                    </a> */}
+                    <Button data={buttonData} className={styles.demo} />
                 </div>
             </div>
         );
